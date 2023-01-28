ca3f55d52e946fc87e160f00ef3625b76564b502
LexHeadko <n-odes11rus@yandex.ru>
2023-01-28 16:57:05 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
+        task2();
         //task3();
         //task4();
         //task5();
@@ -10,9 +10,40 @@ public class Main {
         //task8();
         //task9();
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
 
     public static void task1() {
         System.out.println("Задача 1");
-        int[] arr = new int[9];
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (final int current : arr) {
+            sum += current;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int [] arr = generateRandomArray();
+        int max = 0;
+        for (final int current : arr) {
+            if (max < current) {
+                max = current;
+            }
+        }
+         int min = max;
+        for (final int current : arr) {
+            if (min > current) {
+                min = current;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей.");
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей.");
     }
 }
